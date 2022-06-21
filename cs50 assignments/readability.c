4f70cb62611c59dbeaf4df96a8e8b36e018e5038
ann-yni <103698902+ann-yni@users.noreply.github.com>
2022-06-21 16:56:19 -0700

    Update readability.c

--- cs50 assignments/readability.c
+++ cs50 assignments/readability.c
@@ -1,3 +1,6 @@
+// readability.c utilizes the Coleman-Liau equation for calculating the grade-level of an inputted text. For specifications, view: https://cs50.harvard.edu/x/2022/psets/2/readability/
+
+
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
